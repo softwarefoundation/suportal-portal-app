202590d47ed25482efd4d93423e96876b2e45b0b
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-01-15 08:24:39 -0400

    Add método addUserToLocalCache

--- src/app/service/authetication.service.ts
+++ src/app/service/authetication.service.ts
@@ -40,4 +40,8 @@ export class AutheticationService {
     localStorage.setItem('token',token);
   }
 
+  public addUserToLocalCache(user:User): void {
+    localStorage.setItem('user',JSON.stringify(user));
+  }
+
 }
