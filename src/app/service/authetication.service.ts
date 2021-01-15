c4039446b0ca41cd8df4d2c43eb4c4bb5dd111c4
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-01-15 08:10:25 -0400

    Add método login

--- src/app/service/authetication.service.ts
+++ src/app/service/authetication.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment'
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/User";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutheticationService {
 
-  constructor() { }
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public login (user:User):Observable<HttpResponse<any> | HttpErrorResponse>{
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(
+        `${this.host}/user/login`, user, {observe: 'response'}
+    )
+  }
 }
