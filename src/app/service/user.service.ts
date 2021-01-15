f5f41b9b6d2e30120074f5c8343bdf7206816460
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-01-15 09:27:49 -0400

    Add método getUsers

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/User";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private host = environment.apiUrl
+
+  constructor( private http: HttpClient) { }
+
+  public getUsers(): Observable<User[] | HttpErrorResponse>{
+    return this.http.get<User[]>(`${this.host}/user/list`)
+  }
+
 }
