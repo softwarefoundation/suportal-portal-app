cedf9fa35ae151be665db12dac778ed1004ceb69
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-01-15 09:41:12 -0400

    Add método updateProfileImage

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpEvent} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../model/User";
 
@@ -14,19 +14,26 @@ export class UserService {
   constructor( private http: HttpClient) { }
 
   public getUsers(): Observable<User[] | HttpErrorResponse>{
-    return this.http.get<User[]>(`${this.host}/user/list`)
+    return this.http.get<User[]>(`${this.host}/user/list`);
   }
 
   public addUser(formData: FormData): Observable<User | HttpErrorResponse>{
-    return this.http.post<User>(`${this.host}/user/add`, formData)
+    return this.http.post<User>(`${this.host}/user/add`, formData);
   }
 
   public updateUser(formData: FormData): Observable<User | HttpErrorResponse>{
-    return this.http.post<User>(`${this.host}/user/update`, formData)
+    return this.http.post<User>(`${this.host}/user/update`, formData);
   }
 
   public resetPassword(email:string): Observable<any | HttpErrorResponse>{
-    return this.http.get<User>(`${this.host}/user/resetpassword/${email}`)
+    return this.http.get<User>(`${this.host}/user/resetpassword/${email}`);
+  }
+
+  public updateProfileImage(formData: FormData): Observable<HttpEvent<User> | HttpErrorResponse>{
+    return this.http.post<User>(`${this.host}/user/updateProfileImage`, formData,{
+      reportProgress: true,
+      observe: 'events'
+    });
   }
 
 }
