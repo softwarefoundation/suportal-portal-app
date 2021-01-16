feeb775567ea8c6a846cf63ee27fe7f24e1ae5e4
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-01-16 04:51:02 -0400

    139. Login component - Part 1

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AutheticationService} from "../../service/authetication.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private router:Router,
+              private autheticationService:AutheticationService) { }
 
   ngOnInit(): void {
+    if(this.autheticationService.isLoggedIn()){
+      this.router.navigateByUrl('/user/management');
+    } else {
+      this.router.navigateByUrl('/login')
+    }
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
