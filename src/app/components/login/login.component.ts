d3ae3f01f4fb46371e227e7d1f2aa82bf08ae2d0
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-01-17 08:17:15 -0400

    140. Login component - Part 2

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AutheticationService} from "../../service/authetication.service";
+import {User} from "../../model/User";
+import {Subscription} from "rxjs";
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -8,6 +11,9 @@ import {AutheticationService} from "../../service/authetication.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit, OnDestroy {
+
+  public showLoading: boolean = false;
+  private subscriptions: Subscription[] = [];
 
   constructor(private router:Router,
               private autheticationService:AutheticationService) { }
@@ -18,6 +24,19 @@ export class LoginComponent implements OnInit, OnDestroy {
     } else {
       this.router.navigateByUrl('/login')
     }
+  }
+
+  public onLogin(user:User){
+    this.showLoading = true;
+    console.log(user);
+    this.subscriptions.push(
+        this.autheticationService.login(user).subscribe(
+            //TODO (response HttpResponse<User>) => {
+            (response) => {
+              const token = response.headers.get('Jwt-Token');
+            }
+        )
+    )
   }
 
   ngOnDestroy(): void {
