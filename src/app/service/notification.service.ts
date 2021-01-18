9cf8036617c1da3459acbde49cb40c682a4076ca
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-01-18 00:15:09 -0400

    130. Creating notification service - Correção

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import {NotificationTypeEnum} from "../enum/notification-type.enum";
+import {NotifierService} from "angular-notifier";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor(private notifier: NotificationService) { }
+  constructor(private notifier: NotifierService) { }
 
   public notify(type:NotificationTypeEnum, message:string){
     this.notifier.notify(type, message);
